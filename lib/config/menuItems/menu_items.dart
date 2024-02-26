f65b10188735bb3b62d2f979fa6697fcf8dc84f7
Jose Selma <jlselma25@gmail.com>
2024-02-26 18:21:22 +0100

    creado menu items

---
+++ lib/config/menuItems/menu_items.dart
@@ -0,0 +1,35 @@
+//import 'package:flutter/material.dart' show IconData; con esto indicamos que solo vamos a usar IconDate de material.dart
+
+import 'package:flutter/material.dart';
+
+
+class MenuItems {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItems({
+    required this.title, 
+    required this.subTitle, 
+    required this.link, 
+    required this.icon
+    });
+}
+    //const appMenuItems =<MenuItems>[]; tambien se puede hacer lo mismo que esta abajo
+
+   const List<MenuItems> appMenuItems =[
+
+     MenuItems(
+        title: 'Botonos', 
+        subTitle:'Varios botones en flutter' , 
+        link: '/buttons', 
+        icon: Icons.smart_button_outlined),
+
+        MenuItems(
+        title: 'Tarjetas', 
+        subTitle:'Un contenedor estilizado' , 
+        link: '/card', 
+        icon: Icons.credit_card)
+
+];
