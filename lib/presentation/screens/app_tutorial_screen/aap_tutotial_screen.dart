37d816b6226ea3e597a721e98f79cc8ce9e95374
Jose Selma <jlselma25@gmail.com>
2024-02-28 19:33:12 +0100

    screens create

---
+++ lib/presentation/screens/app_tutorial_screen/aap_tutotial_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ApptutorialScreen extends StatelessWidget {
+
+    static const String name = 'tutorial_screen';
+  const ApptutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
